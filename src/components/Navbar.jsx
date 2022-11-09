56f2bd6b041b680c8bfb9d4aa6db90e018f1cce2
Whalay <soliuy5@gmail.com>
2022-11-09 15:49:04 +0100

    refactor the  nav section

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,7 +16,7 @@ const Navbar = () => {
     <section className="flex justify-between md:px-20  p-5 items-center">
       <div className="flex gap-10">
         <img src={logo} alt="" />
-        <ul className="flex gap-5">
+        <ul className="hidden md:flex gap-5">
           <li>Features</li>
           <li>Prices</li>
           <li>Resources</li>
@@ -25,7 +25,7 @@ const Navbar = () => {
 
       <div className="flex ">
         <GiHamburgerMenu onClick={navHandler} className="w-10 h-10 md:hidden" />
-        <ul className="flex gap-5 items-center">
+        <ul className="md:flex gap-5 items-center hidden">
           <li>Login</li>
           <li className="bg-Cyan px-5 py-3  font-semi-bold   text-white rounded-full ">Sign Up</li>
         </ul>
