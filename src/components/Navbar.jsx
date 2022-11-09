a467d3cad4cba611c7995eaae12d0b8a4a860fa9
Whalay <soliuy5@gmail.com>
2022-11-09 15:44:25 +0100

    added the nav section

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,17 +9,29 @@ const Navbar = () => {
   const [showNav, setShowNav] = useState(false);
 
   const navHandler = () => {
-    setShowNav((showNav) => (!showNav));
+    setShowNav((showNav) => !showNav);
   };
 
   return (
-    <section className="flex justify-between md:px-20  p-5">
-      
-      <img src={logo} alt="" />
+    <section className="flex justify-between md:px-20  p-5 items-center">
+      <div className="flex gap-10">
+        <img src={logo} alt="" />
+        <ul className="flex gap-5">
+          <li>Features</li>
+          <li>Prices</li>
+          <li>Resources</li>
+        </ul>
+      </div>
 
-      <GiHamburgerMenu onClick={navHandler} className="w-10 h-10" />
+      <div className="flex ">
+        <GiHamburgerMenu onClick={navHandler} className="w-10 h-10 md:hidden" />
+        <ul className="flex gap-5 items-center">
+          <li>Login</li>
+          <li className="bg-Cyan px-5 py-3  font-semi-bold   text-white rounded-full ">Sign Up</li>
+        </ul>
+      </div>
 
-     {showNav &&  <MobileNav navHandler ={navHandler} />}
+      {showNav && <MobileNav navHandler={navHandler} />}
     </section>
   );
 };
